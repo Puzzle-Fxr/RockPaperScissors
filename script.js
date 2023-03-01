7f4dfdba04f6540ab0f4d34ba235cc51b3813ce8
Puzzle-Fxr <caldwellavogo@gmail.com>
2023-03-01 18:46:21 +0000

    complete one round function

--- script.js
+++ script.js
@@ -11,7 +11,15 @@ console.log("hello!");
 console.log(getComputerChoice());
 
 function playRound (playerSelection, computerSelection){
+    if (playerSelection === "Rock" && computerSelection === "Scissors" 
+    || playerSelection === "Paper" && computerSelection === "Rock"
+    || playerSelection === "Scissorss" && computerSelection === "Paper") {
+        return "You Win!";
+    } else if (playerSelection === computerSelection){
+        return "Its a Draw!"
+    } else return "You Lose!";
 }
 
 let playerSelection = "Rock";
 let computerSelection = getComputerChoice();
+console.log (playRound(playerSelection, computerSelection));
