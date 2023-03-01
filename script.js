58752bdd9682f645c08a07cc7261cba68e3aca50
Puzzle-Fxr <caldwellavogo@gmail.com>
2023-03-01 18:03:07 +0000

    remove argumennts from pcChoice function

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 /*Script*/
-const pcChoice = ['Rock', 'Paper', 'Scissors'];
+/*const pcChoice = ['Rock', 'Paper', 'Scissors'];*/
 
-function getComputerChoice (choice){
-    let random = Math.floor (Math.random() * choice.length);
-    return choice[random];
+function getComputerChoice (){
+    const pcChoice = ['Rock', 'Paper', 'Scissors'];
+    let random = Math.floor (Math.random() * pcChoice.length);
+    return pcChoice[random];
 }
 
 console.log("hello!");
-console.log(getComputerChoice(pcChoice));
+console.log(getComputerChoice());
+
+function playRound (playerSelection, computerSelection){
+}
+
+let playerSelection = "Rock";
+let computerSelection = getComputerChoice();
