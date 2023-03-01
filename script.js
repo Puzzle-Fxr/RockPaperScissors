9503736f7021dc2383f63c7da85d4b7fbe52135c
Puzzle-Fxr <caldwellavogo@gmail.com>
2023-03-01 22:39:02 +0000

    play one round

--- script.js
+++ script.js
@@ -2,24 +2,30 @@
 /*const pcChoice = ['Rock', 'Paper', 'Scissors'];*/
 
 function getComputerChoice (){
-    const pcChoice = ['Rock', 'Paper', 'Scissors'];
+    const pcChoice = ['rock', 'paper', 'scissors'];
     let random = Math.floor (Math.random() * pcChoice.length);
     return pcChoice[random];
 }
 
-console.log("hello!");
-console.log(getComputerChoice());
-
 function playRound (playerSelection, computerSelection){
-    if (playerSelection === "Rock" && computerSelection === "Scissors" 
-    || playerSelection === "Paper" && computerSelection === "Rock"
-    || playerSelection === "Scissorss" && computerSelection === "Paper") {
-        return "You Win!";
-    } else if (playerSelection === computerSelection){
-        return "Its a Draw!"
-    } else return "You Lose!";
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors cuts paper into pieces!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock smashes scissors into pieces!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper wraps rock into submission!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock smashes scissors into pieces!";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper wraps rock into submission!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! Scissors cuts paper into pieces!";
+    } else if (playerSelection === computerSelection) {
+        return "Its a Draw! Play again?"
+    } else return "Wrong cast, please type 'rock', 'paper' or 'scissors'.";
 }
 
-let playerSelection = "Rock";
+let playerChoice = "PaPER";
+let playerSelection = playerChoice.toLowerCase();
 let computerSelection = getComputerChoice();
 console.log (playRound(playerSelection, computerSelection));
