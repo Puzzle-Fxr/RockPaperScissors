55b2bd7082919e1af36e9e68f469c7d886665e18
Puzzle-Fxr <caldwellavogo@gmail.com>
2023-06-27 20:18:48 +0000

    count player wins

--- script.js
+++ script.js
@@ -7,12 +7,28 @@ function getComputerChoice (){
     return pcChoice[random];
 }
 
+const score = document.querySelector('#score');
+const wins = document.querySelector('#playerWins');
+
+let playerSelection = document.querySelectorAll('.btn');
+playerSelection.forEach((btn)=> btn.addEventListener('click', ()=> {
+    score.textContent = playRound(btn.id, getComputerChoice());
+    playerWins.textContent = playerWin;
+}));
+
+let playerWin = 0;
+let pcWin = 0;
+let draw = 0;
+
 function playRound (playerSelection, computerSelection){
     if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerWin ++;
         return "You Win! Scissors cuts paper into pieces!";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerWin ++;
         return "You Win! Rock smashes scissors into pieces!";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerWin ++;
         return "You Win! Paper wraps rock into submission!";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         return "You Lose! Rock smashes scissors into pieces!";
@@ -25,14 +41,4 @@ function playRound (playerSelection, computerSelection){
     } else return "Wrong cast, please type 'rock', 'paper' or 'scissors'.";
 }
 
-let playerWin = 0;
-let pcWin = 0;
-let draw = 0;
-
-const score = document.querySelector('#score');
-
-let playerSelection = document.querySelectorAll('.btn');
-playerSelection.forEach((btn)=> btn.addEventListener('click', ()=> {
-    score.textContent = playRound(btn.id, getComputerChoice());
-}));
 
