6c1dd699b8e6278dcbc4ecc9cc7f8c47cbc530c1
Puzzle-Fxr <caldwellavogo@gmail.com>
2023-06-30 14:17:46 +0000

    add picture result, add color to UI

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ const tie = document.querySelector('#draws');
 let playerSelection = document.querySelectorAll('.btn');
 playerSelection.forEach((btn)=> btn.addEventListener('click', ()=> {
     score.textContent = playRound(btn.id, getComputerChoice());
-    wins.textContent = "Player Wins ="+playerWin;
-    pwins.textContent = "Cpu Wins ="+pcWin;
-    tie.textContent = "Draws ="+draw;
+    wins.textContent = "Player Wins = "+playerWin;
+    pwins.textContent = "Cpu Wins = "+pcWin;
+    tie.textContent = "Draws = "+draw;
     declareWin();
 }));
 
@@ -33,30 +33,63 @@ function declareWin (){
     };
 };
 
+const vis = document.getElementsByClassName('vis');
+const visOne = document.getElementById('visOne');
+const visTwo = document.getElementById('visTwo');
+const visThree = document.getElementById('visThree');
+const visFour = document.getElementById('visFour');
 
 function playRound (playerSelection, computerSelection){
     if (playerSelection === "scissors" && computerSelection === "paper") {
         playerWin ++;
+        visOne.style.visibility = 'hidden';
+        visTwo.style.visibility = 'visible';
+        visThree.style.visibility = 'hidden';
+        visFour.style.visibility = 'hidden';
         return "You Win! Scissors cuts paper into pieces!";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         playerWin ++;
+        visOne.style.visibility = 'visible';
+        visTwo.style.visibility = 'hidden';
+        visThree.style.visibility = 'hidden';
+        visFour.style.visibility = 'hidden';
         return "You Win! Rock smashes scissors into pieces!";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         playerWin ++;
+        visOne.style.visibility = 'hidden';
+        visTwo.style.visibility = 'hidden';
+        visThree.style.visibility = 'visible';
+        visFour.style.visibility = 'hidden';
         return "You Win! Paper wraps rock into submission!";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         pcWin ++;
+        visOne.style.visibility = 'visible';
+        visTwo.style.visibility = 'hidden';
+        visThree.style.visibility = 'hidden';
+        visFour.style.visibility = 'hidden';
         return "You Lose! Rock smashes scissors into pieces!";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         pcWin ++;
+        visOne.style.visibility = 'hidden';
+        visTwo.style.visibility = 'hidden';
+        visThree.style.visibility = 'visible';
+        visFour.style.visibility = 'hidden';
         return "You Lose! Paper wraps rock into submission!";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         pcWin ++;
+        visOne.style.visibility = 'hidden';
+        visTwo.style.visibility = 'visible';
+        visThree.style.visibility = 'hidden';
+        visFour.style.visibility = 'hidden';
         return "You Lose! Scissors cuts paper into pieces!";
     } else if (playerSelection === computerSelection) {
         draw ++;
+        visOne.style.visibility = 'hidden';
+        visTwo.style.visibility = 'hidden';
+        visThree.style.visibility = 'hidden';
+        visFour.style.visibility = 'visible';
         return "Its a tie!";
     } else return "Wrong cast, please type 'rock', 'paper' or 'scissors'.";
-}
+};
 
 
